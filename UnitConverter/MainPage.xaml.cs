7f8ce5b95aaa32dd59e87789da436ed8c4e20735
Jan <honzasrba@gmail.com>
2022-10-06 21:38:42 +0200

    Temperature units added. Slightly refactored

--- UnitConverter/MainPage.xaml.cs
+++ UnitConverter/MainPage.xaml.cs
@@ -3,6 +3,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using UnitConverter.Model;
 
 namespace UnitConverter;
 
@@ -15,6 +16,17 @@ public partial class MainPage : ContentPage
         InitializeAreaElements();
         InitializeVolumeElements();
         InitializeMassElements();
+        InitializeTemperatureElements();
+    }
+
+    public void InitializePickers(Picker upperPicker, Picker lowerPicker, List<PhysicalUnit> units, string upperSelected, string lowerSelected)
+    {
+        upperPicker.ItemsSource = units;
+        upperPicker.ItemDisplayBinding = new Binding("Name");
+        upperPicker.SelectedItem = (PhysicalUnit)units.Where(x => x.Name == upperSelected).First();
+        lowerPicker.ItemsSource = units;
+        lowerPicker.ItemDisplayBinding = new Binding("Name");
+        lowerPicker.SelectedItem = (PhysicalUnit)units.Where(x => x.Name == lowerSelected).First();
     }
 
     public void InitializeLengthElements()
@@ -28,16 +40,17 @@ public partial class MainPage : ContentPage
         lowerLengthPicker.SelectedItem = (PhysicalUnit)lengthUnits.Where(x => x.Name == "inch").First();
     }
 
-    public void InitializeAreaElements()
+    public void InitializeTemperatureElements()
     {
-        List<PhysicalUnit> areaUnits = PhysicalUnit.GetAreaUnits();
-        upperAreaPicker.ItemsSource = areaUnits;
-        upperAreaPicker.ItemDisplayBinding = new Binding("Name");
-        upperAreaPicker.SelectedItem = (PhysicalUnit)areaUnits.Where(x => x.Name == "square meter").First();
-        lowerAreaPicker.ItemsSource = areaUnits;
-        lowerAreaPicker.ItemDisplayBinding = new Binding("Name");
-        lowerAreaPicker.SelectedItem = (PhysicalUnit)areaUnits.Where(x => x.Name == "acre").First();
+        List<PhysicalUnit> temperatureUnits = PhysicalUnit.GetTemperatureUnits();
+        upperTemperaturePicker.ItemsSource = temperatureUnits;
+        upperTemperaturePicker.ItemDisplayBinding = new Binding("Name");
+        upperTemperaturePicker.SelectedItem = (PhysicalUnit)temperatureUnits.Where(x => x.Name == "degree Celsius").First();
+        lowerTemperaturePicker.ItemsSource = temperatureUnits;
+        lowerTemperaturePicker.ItemDisplayBinding = new Binding("Name");
+        lowerTemperaturePicker.SelectedItem = (PhysicalUnit)temperatureUnits.Where(x => x.Name == "degree Fahrenheit").First();
     }
+
 
     public void InitializeVolumeElements()
     {
@@ -59,6 +72,17 @@ public partial class MainPage : ContentPage
         lowerMassPicker.ItemsSource = massUnits;
         lowerMassPicker.ItemDisplayBinding = new Binding("Name");
         lowerMassPicker.SelectedItem = (PhysicalUnit)massUnits.Where(x => x.Name == "pound").First();
+    }
+
+    public void InitializeAreaElements()
+    {
+        List<PhysicalUnit> areaUnits = PhysicalUnit.GetAreaUnits();
+        upperAreaPicker.ItemsSource = areaUnits;
+        upperAreaPicker.ItemDisplayBinding = new Binding("Name");
+        upperAreaPicker.SelectedItem = (PhysicalUnit)areaUnits.Where(x => x.Name == "square meter").First();
+        lowerAreaPicker.ItemsSource = areaUnits;
+        lowerAreaPicker.ItemDisplayBinding = new Binding("Name");
+        lowerAreaPicker.SelectedItem = (PhysicalUnit)areaUnits.Where(x => x.Name == "acre").First();
     }
 
 
@@ -86,7 +110,14 @@ public partial class MainPage : ContentPage
         {
             lowerLabel.Text = "Incorrect unit selected";
             return;
-        }     
+        }
+        if (lowerUnit.RelationToBaseWithConstant != null || lowerUnit.RelationToBaseWithConstant != null)
+        {
+            lowerLabel.Text = Math.Round(
+                (enteredValue * upperUnit.RelationToBaseWithConstant[0] + upperUnit.RelationToBaseWithConstant[1]) / lowerUnit.RelationToBaseWithConstant[0] -
+                lowerUnit.RelationToBaseWithConstant[1] / lowerUnit.RelationToBaseWithConstant[0], 12).ToString();
+            return;
+        }
         lowerLabel.Text = Math.Round(enteredValue * upperUnit.RelationToBase / lowerUnit.RelationToBase, 12).ToString();
     }
 
@@ -129,94 +160,15 @@ public partial class MainPage : ContentPage
     {
         ConvertUnits(upperLengthEntry, upperLengthPicker, lowerLengthPicker, lowerLengthLabel);
     }
-}
 
-public class PhysicalUnit
-{
-    public string Name { get; set; }
-    public string Abbreviation { get; set; }
-    public string UnitSystem { get; set; }
-    public double RelationToBase { get; set; }
-
-    public PhysicalUnit(string name, string abbreviation, string unitSystem, double relationToBase)
+    private void upperTemperatureEntry_TextChanged(object sender, TextChangedEventArgs e)
     {
-        Name = name;
-        Abbreviation = abbreviation;
-        UnitSystem = unitSystem;
-        RelationToBase = relationToBase;
+        ConvertUnits(upperTemperatureEntry, upperTemperaturePicker, lowerTemperaturePicker, lowerTemperatureLabel);
     }
 
-
-    public static List<PhysicalUnit> GetLengthUnits()
+    private void upperTemperaturePicker_SelectedIndexChanged(object sender, EventArgs e)
     {
-        List<PhysicalUnit> lengthUnits = new List<PhysicalUnit>() {
-            new PhysicalUnit("meter", "m", "metric", 1),
-            new PhysicalUnit("decimeter", "dm", "metric", 0.1),
-            new PhysicalUnit("centimeter", "cm", "metric", 0.01),
-            new PhysicalUnit("milimeter", "mm", "metric", 0.001),
-            new PhysicalUnit("kilometer", "km", "metric", 1000),
-            new PhysicalUnit("inch", "in", "imperial", 0.0254),
-            new PhysicalUnit("foot", "ft", "imperial", 0.3048),
-            new PhysicalUnit("yard", "yd", "imperial", 0.9144),
-            new PhysicalUnit("mile", "mi", "imperial", 1609.344)
-        };
-        return lengthUnits;
-    }
-
-    public static List<PhysicalUnit> GetAreaUnits()
-    {
-        List<PhysicalUnit> areaUnits = new List<PhysicalUnit>() {
-            new PhysicalUnit("square meter", "m^2", "metric", 1),
-            new PhysicalUnit("square decimeter", "dm^2", "metric", 0.01),
-            new PhysicalUnit("square centimeter", "cm^2", "metric", 0.0001),
-            new PhysicalUnit("square milimeter", "mm^2", "metric", 0.000001),
-            new PhysicalUnit("ar", "ar", "metric", 100),
-            new PhysicalUnit("hectar", "ha", "metric", 10000),
-            new PhysicalUnit("square kilometer", "km^2", "metric", 1000000),
-            new PhysicalUnit("perch", "perch", "imperial", 25.29285264),
-            new PhysicalUnit("rood", "rood", "imperial", 1011.7141056),
-            new PhysicalUnit("acre", "acre", "imperial", 4046.8564224),
-            new PhysicalUnit("square mile", "sq mi", "imperial", 2589988.110336)
-    };
-        return areaUnits;
-    }
-
-    public static List<PhysicalUnit> GetVolumeUnits()
-    {
-        List<PhysicalUnit> volumeUnits = new List<PhysicalUnit>() {
-            new PhysicalUnit("liter", "l", "metric", 1),
-            new PhysicalUnit("deciliter", "dl", "metric", 0.1),
-            new PhysicalUnit("centiliter", "l", "metric", 0.01),
-            new PhysicalUnit("mililiter", "ml", "metric", 0.001),
-            new PhysicalUnit("hectoliter", "hl", "metric", 100),
-            new PhysicalUnit("cubic meter", "m^3", "metric", 1000),
-            new PhysicalUnit("cubic inch", "cu in", "imperial", 0.016387064),
-            new PhysicalUnit("fluid ounce", "fl oz", "imperial", 0.0284130625),
-            new PhysicalUnit("gill", "gi", "imperial", 0.1420653125),
-            new PhysicalUnit("pint", "pt", "imperial", 0.56826125),
-            new PhysicalUnit("quart", "qt", "imperial", 1.1365225),
-            new PhysicalUnit("gallon", "gal", "imperial", 4.54609)
-        };
-        return volumeUnits;
-    }
-
-    public static List<PhysicalUnit> GetMassUnits()
-    {
-        List<PhysicalUnit> massUnits = new List<PhysicalUnit>() {
-            new PhysicalUnit("kilogram", "kg", "metric", 1),
-            new PhysicalUnit("decagram", "dg", "metric", 0.01),
-            new PhysicalUnit("gram", "g", "metric", 0.001),
-            new PhysicalUnit("miligram", "mg", "metric", 0.000001),
-            new PhysicalUnit("metric ton", "t", "metric", 1000),
-            new PhysicalUnit("grain", "gr", "imperial", 0.00006479891),
-            new PhysicalUnit("drachm", "dr", "imperial", 0.0017718451953125),
-            new PhysicalUnit("ounce", "oz", "imperial", 0.028349523125),
-            new PhysicalUnit("pound", "lb", "imperial", 0.45359237),
-            new PhysicalUnit("stone", "st", "imperial", 6.35029318),
-            new PhysicalUnit("quarter", "qtr", "imperial", 12.70058636),
-            new PhysicalUnit("hundredweight", "cwt", "imperial", 50.80234544),
-            new PhysicalUnit("(imperial) ton", "t", "imperial", 1016.0469088)
-        };
-        return massUnits;
+        ConvertUnits(upperTemperatureEntry, upperTemperaturePicker, lowerTemperaturePicker, lowerTemperatureLabel);
     }
 }
+
---
+++ UnitConverter/Model/PhysicalUnit.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter.Model
+{
+    public class PhysicalUnit
+    {
+        public enum UnitType
+        {
+            Length,
+            Temperature,
+            Volume,
+            Mass,
+            Area
+        }
+
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+        public UnitType Type { get; set; }
+        public double RelationToBase { get; set; }
+        public double[] RelationToBaseWithConstant { get; set; }
+
+        public PhysicalUnit(string name, string abbreviation, UnitType unitType, double relationToBase)
+        {
+            Name = name;
+            Abbreviation = abbreviation;
+            Type = unitType;
+            RelationToBase = relationToBase;
+        }
+
+        public PhysicalUnit(string name, string abbreviation, UnitType unitType, double[] relationToBaseWithConstant)
+        {
+            Name = name;
+            Abbreviation = abbreviation;
+            Type = unitType;
+            RelationToBaseWithConstant = relationToBaseWithConstant;
+        }
+
+
+        public static List<PhysicalUnit> GetLengthUnits()
+        {
+            List<PhysicalUnit> lengthUnits = new List<PhysicalUnit>() {
+            new PhysicalUnit("meter", "m", UnitType.Length, 1),
+            new PhysicalUnit("decimeter", "dm", UnitType.Length, 0.1),
+            new PhysicalUnit("centimeter", "cm", UnitType.Length, 0.01),
+            new PhysicalUnit("milimeter", "mm", UnitType.Length, 0.001),
+            new PhysicalUnit("kilometer", "km", UnitType.Length, 1000),
+            new PhysicalUnit("inch", "in", UnitType.Length, 0.0254),
+            new PhysicalUnit("foot", "ft", UnitType.Length, 0.3048),
+            new PhysicalUnit("yard", "yd", UnitType.Length, 0.9144),
+            new PhysicalUnit("mile", "mi", UnitType.Length, 1609.344)
+        };
+            return lengthUnits;
+        }
+
+        public static List<PhysicalUnit> GetTemperatureUnits()
+        {
+            List<PhysicalUnit> temperatureUnits = new List<PhysicalUnit>() {
+            new PhysicalUnit("degree Celsius", "°C", UnitType.Temperature, new double[2]{1, 0}),
+            new PhysicalUnit("degree Fahrenheit", "°F", UnitType.Temperature, new double[2]{5.0/9.0, -32*5.0/9.0}),
+        };
+            return temperatureUnits;
+        }
+
+
+        public static List<PhysicalUnit> GetVolumeUnits()
+        {
+            List<PhysicalUnit> volumeUnits = new List<PhysicalUnit>() {
+            new PhysicalUnit("liter", "l", UnitType.Volume, 1),
+            new PhysicalUnit("deciliter", "dl", UnitType.Volume, 0.1),
+            new PhysicalUnit("centiliter", "l", UnitType.Volume, 0.01),
+            new PhysicalUnit("mililiter", "ml", UnitType.Volume, 0.001),
+            new PhysicalUnit("hectoliter", "hl", UnitType.Volume, 100),
+            new PhysicalUnit("cubic meter", "m^3", UnitType.Volume, 1000),
+            new PhysicalUnit("cubic inch", "cu in", UnitType.Volume, 0.016387064),
+            new PhysicalUnit("fluid ounce", "fl oz", UnitType.Volume, 0.0284130625),
+            new PhysicalUnit("gill", "gi", UnitType.Volume, 0.1420653125),
+            new PhysicalUnit("pint", "pt", UnitType.Volume, 0.56826125),
+            new PhysicalUnit("quart", "qt", UnitType.Volume, 1.1365225),
+            new PhysicalUnit("gallon", "gal", UnitType.Volume, 4.54609)
+        };
+            return volumeUnits;
+        }
+
+        public static List<PhysicalUnit> GetMassUnits()
+        {
+            List<PhysicalUnit> massUnits = new List<PhysicalUnit>() {
+            new PhysicalUnit("kilogram", "kg", UnitType.Mass, 1),
+            new PhysicalUnit("decagram", "dg", UnitType.Mass, 0.01),
+            new PhysicalUnit("gram", "g", UnitType.Mass, 0.001),
+            new PhysicalUnit("miligram", "mg", UnitType.Mass, 0.000001),
+            new PhysicalUnit("metric ton", "t", UnitType.Mass, 1000),
+            new PhysicalUnit("grain", "gr", UnitType.Mass, 0.00006479891),
+            new PhysicalUnit("drachm", "dr", UnitType.Mass, 0.0017718451953125),
+            new PhysicalUnit("ounce", "oz", UnitType.Mass, 0.028349523125),
+            new PhysicalUnit("pound", "lb", UnitType.Mass, 0.45359237),
+            new PhysicalUnit("stone", "st", UnitType.Mass, 6.35029318),
+            new PhysicalUnit("quarter", "qtr", UnitType.Mass, 12.70058636),
+            new PhysicalUnit("hundredweight", "cwt", UnitType.Mass, 50.80234544),
+            new PhysicalUnit("(imperial) ton", "t", UnitType.Mass, 1016.0469088)
+        };
+            return massUnits;
+        }
+
+        public static List<PhysicalUnit> GetAreaUnits()
+        {
+            List<PhysicalUnit> areaUnits = new List<PhysicalUnit>() {
+            new PhysicalUnit("square meter", "m^2", UnitType.Area, 1),
+            new PhysicalUnit("square decimeter", "dm^2", UnitType.Area, 0.01),
+            new PhysicalUnit("square centimeter", "cm^2", UnitType.Area, 0.0001),
+            new PhysicalUnit("square milimeter", "mm^2", UnitType.Area, 0.000001),
+            new PhysicalUnit("ar", "ar", UnitType.Area, 100),
+            new PhysicalUnit("hectar", "ha", UnitType.Area, 10000),
+            new PhysicalUnit("square kilometer", "km^2", UnitType.Area, 1000000),
+            new PhysicalUnit("perch", "perch", UnitType.Area, 25.29285264),
+            new PhysicalUnit("rood", "rood", UnitType.Area, 1011.7141056),
+            new PhysicalUnit("acre", "acre", UnitType.Area, 4046.8564224),
+            new PhysicalUnit("square mile", "sq mi", UnitType.Area, 2589988.110336)
+    };
+            return areaUnits;
+        }
+
+
+    }
+
+}
